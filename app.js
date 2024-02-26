ee1c930ced19fa7d74b2efe7e8c513924dd48ecc
l.gamezardashvili <l.gamezardashvili@rs.ge>
2024-02-26 17:18:10 +0400

    cpomm

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 let position = 0;
 let marioStart = document.getElementById('mario_Start');
 let mario = document.getElementById('mario');
+let start_button = document.getElementById('startButton');
+let details = document.querySelector('.personal-details');
+
+
 const maxPosition = window.innerWidth > 2560 ? 2000 : window.innerWidth;
 let isRunning = false;
-
+details.style.display = 'none';
 
 document.addEventListener("DOMContentLoaded", function () {
-    marioStart.style.display = 'block'; 
-    mario.style.display = 'none'; 
+    marioStart.style.display = 'block';
+    mario.style.display = 'none';
+
 });
 
 function moveMario() {
@@ -16,55 +21,32 @@ function moveMario() {
     if (position > maxPosition) {
         position = mario.width - 40;
         isRunning = false;
-        marioStart.style.display = 'block'; 
-        mario.style.display = 'none'; 
-    } 
+        marioStart.style.display = 'block';
+        mario.style.display = 'none';
+        start_button.style.display = 'inline';
+    }
 
     mario.style.left = position + 'px';
 
     if (isRunning) {
+        start_button.style.display = 'none';
+        details.style.display = 'inline';
+
         requestAnimationFrame(moveMario);
     }
 }
 
 document.getElementById('startButton').addEventListener('click', function () {
     if (!isRunning) {
-        position = 0; 
+        position = 0;
         marioStart.style.display = 'none';
         mario.style.display = 'block';
         isRunning = true;
-        document.getElementById('startButton').style.display = 'none';
+        start_button.style.display = 'inline';
         moveMario();
     }
+
 });
 
 
 
-//moveMario();
-
-
-
-let isMoving = false;
-        let initialX = 0;
-
-        function startMove(event) {
-            isMoving = true;
-            initialX = event.clientX;
-        }
-
-        function stopMove() {
-            isMoving = false;
-        }
-
-        document.addEventListener('mousemove', moveDetails);
-
-        function moveDetails(event) {
-            if (isMoving) {
-                const movement = event.clientX - initialX;
-                const details = document.querySelector('.personal-details');
-
-                const multiplier = 0.1;
-                const currentMargin = parseInt(getComputedStyle(details).marginLeft);
-                details.style.marginLeft = currentMargin + multiplier * movement + 'px';
-            }
-        }
